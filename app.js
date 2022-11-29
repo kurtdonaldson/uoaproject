7cfc22a1792cc1fbb94b88d8489a8a01f379808d
DONNY345 <89885417+DONNY345@users.noreply.github.com>
2022-11-30 10:39:59 +1300

    port changes for deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Setup Express
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const methodOverride = require("method-override");
 
 //Using method override to allow use of put request on client side. Client side only accepts get or post requests
