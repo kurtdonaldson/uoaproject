a4cb2042595251e8b53ffb4e3f9a715ce0c1d266
kurtdonaldson <89885417+kurtdonaldson@users.noreply.github.com>
2022-11-30 20:30:17 +1300

    database

--- modules/databasepg.js
+++ modules/databasepg.js
@@ -14,3 +14,35 @@ const client = new Client({
 client.connect();
 
 module.exports = client;
+
+// EMAIL_USERNAME=iguanas22@zohomail.com
+// EMAIL_PASSWORD=tDBJzqJ3MqbT
+// DOMAIN=localhost:3000
+// POSTGRES_PASSWORD=Auckland7
+// PORT = 
+// PGUSER=blog_database_lu9i_user
+// PGHOST=postgres://blog_database_lu9i_user:5bDVJiJYtxpEzkEfQXsTZQKfMj3bctib@dpg-ce37supa6gdlrbdesmp0-a.oregon-postgres.render.com/blog_database_lu9i
+// PGPASSWORD=5bDVJiJYtxpEzkEfQXsTZQKfMj3bctib
+// PGDATABASE=blog_database_lu9i
+// PGPORT=5432
+// DATABASE_URL=postgres://blog_database_lu9i_user:5bDVJiJYtxpEzkEfQXsTZQKfMj3bctib@dpg-ce37supa6gdlrbdesmp0-a/blog_database_lu9i
+// NODE_ENV=production
+
+// const dotenv = require("dotenv");
+// dotenv.config({path: "../.env"});
+
+
+// const {Client} = require('pg');
+
+
+// const client = new Client({
+//     host: "localhost",
+//     user: "postgres",
+//     port: 5432,
+//     password: 'Auckland7',
+//     database: "blog-project"
+// })
+
+// client.connect();
+
+// module.exports = client;
