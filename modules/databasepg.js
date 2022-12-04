f7d5fab13255769e82a04849cc83aa17dce9fb08
DONNY345 <89885417+DONNY345@users.noreply.github.com>
2022-12-05 12:55:10 +1300

    Connection env added

--- modules/databasepg.js
+++ modules/databasepg.js
@@ -4,7 +4,7 @@ dotenv.config();
 const {Client} = require('pg');
 
 
-const conectionString='postgres://blog_database_lu9i_user:5bDVJiJYtxpEzkEfQXsTZQKfMj3bctib@dpg-ce37supa6gdlrbdesmp0-a.oregon-postgres.render.com/blog_database_lu9i?ssl=true';
+const conectionString = process.env.CONNECTION;
 const client = new Client({
     connectionString: conectionString
     })
